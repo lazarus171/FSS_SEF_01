aa46ff98a633c1afdacfb928ab632c0dcc479d73
lazarus171 <64349556+lazarus171@users.noreply.github.com>
2023-02-23 11:28:24 +0100

    23 febbraio 2023 - Modificato il file di setup per cambio dei file da cui recuper i dati di prova. - Modificato la funzione ch_filter per marcare il flag dei canali mantenendo marcature precedenti.

--- ch_filter.m
+++ ch_filter.m
@@ -3,17 +3,19 @@ function Flag = ch_filter(ch_Flag, ch_struct)
 %  Segna con uno 0 i canali non utili nel ch_flag
 %  Per semplicità si utilizzano due stringhe per i possibili nomi di canali
 %  da mantenere, tutti gli altri sono considerati da eliminare
+%Comunicazioni per l'utente
+uiwait(msgbox({'Questa funzione marca con uno zero il flag', 'di tutti i canali NON di tipo MEG o EEG', 'mantenendo eventuali zeri già presenti.'},'Attenzione!','warn', 'modal'));
+%Lista dei tipi di canale da marcare
 keep = {'MEG', 'EEG'};
 %Ciclo di scansione del ch_flag
 for i = 1 : length(ch_Flag)
-    %Ciclo di scansione della lista da mantenere
-    for j = 1 : length(keep)
-        %Imposta il flag del canale sul risultato della comparazione fra le
-        %stringhe
-        if strcmp(ch_struct(i).Type, keep(j))
-            ch_Flag(i) = 1;
-        end
-    end
+    s1 = ch_struct(i).Type;
+    %Condizione di marcatura
+    marker = sum(strcmp(s1, keep));
+    %Marca il flag moltiplicando il marcatore per il valore del flag stesso
+    %per mantenere gli zeri eventualmente già presenti(es. un canale già
+    %marcato in altra sede)
+    ch_Flag(i) = marker * ch_Flag(i);
 end
 Flag = ch_Flag;
 end
--- setup.m
+++ setup.m
@@ -3,9 +3,10 @@
 clear;
 %Carica le strutture e le variabili dal file appositamente predisposto
 load('struct_setup.mat');
-%Apre la finestra di dialogo per scegliere il file dei dati
+%Apre la finestra di dialogo per scegliere il file dei dati; il file
+%proposto di default serve x sveltire le operazioni di test
 [f_name, f_path] = uigetfile('','Seleziona il file contenente le registrazioni dei dati',...
-    'D:\brainstorm_db\TutorialCMC\data\Subject01\SubjectCMC_notch_high_abs\data_Left_average_230214_1734.mat');
+    'D:\brainstorm_db\TutorialCMC\data\Subject01\SubjectCMC_notch_02\data_block001.mat');
 %Imposta il valore della variabile DataPath in base alla scelta fatta
 %controllando che la scelta sia valida
 if f_name == 0
@@ -13,10 +14,10 @@ if f_name == 0
 else
     DataPath = fullfile(f_path, f_name);
 end
-%Apre la finestra di dialogo per scegliere il file dei canali proponendo il
-%file dati come default
+%Apre la finestra di dialogo per scegliere il file dei canali; il file
+%proposto di default serve x sveltire le operazioni di test
 [f_name, f_path] = uigetfile('', 'Seleziona il file contenente la descrizione dei canali',...
-    'D:\brainstorm_db\TutorialCMC\data\Subject01\SubjectCMC_notch_high_abs\channel_ctf_acc1.mat');
+    'D:\brainstorm_db\TutorialCMC\data\Subject01\SubjectCMC_notch_02\channel_ctf_acc1.mat');
 %Imposta il valore della variabile ChanlocsPath in base alla scelta fatta
 %controllando che la scelta sia valida
 if f_name == 0
